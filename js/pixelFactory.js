6995055b91a8d1beccd252bf67e224613e0be278
BraddahBoots <bryan@thebrocode.io>
2015-09-19 12:25:25 -1000

    set architicture of pixelFactory

---
+++ js/pixelFactory.js
@@ -0,0 +1,13 @@
+
+//set this. pixel color to palette color 'purple'
+function changeColor() {
+
+}
+
+//create a div pixel when invoked
+//pixel will have background color property - default white
+//will have a height and width property
+//addEventListener property ('click', changeColor())
+function pixelFactor() {
+
+}

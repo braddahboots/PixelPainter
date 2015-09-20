cb7f74be3f33ac66bebb63d086c55a5c2058e059
BraddahBoots <bryan@thebrocode.io>
2015-09-19 16:11:08 -1000

    Created a main grid that is able to change color bassed on our selected palette

--- js/mainGrid.js
+++ js/mainGrid.js
@@ -1,9 +1,30 @@
-
 //setup blank canvas with changeable pixels
 function pixelPainter(width, height) {
-  // width = row , height = columns
+  // width = colums , height = row
   // matrix of rows * columns
   // populate with new Pixel --- pixel factory
   // append each pixel to a unique row
   // append row to canvas --- pixelPainter
+var rows = height;
+var cols = width;
+var row;
+var pix;
+var grid = document.createElement('table');
+grid.id = 'grid';
+
+
+for(var i = 0; i < rows; i++) {
+  row = document.createElement('tr');
+  row.className = 'row';
+  for(var j = 0; j < cols; j++) {
+    pix = pixelFactory();
+    row.appendChild(pix);
+  }
+  grid.appendChild(row);
 }
+
+document.body.appendChild(grid);
+// debugger;
+console.log("test", document.body.appendChild(grid));
+}
+pixelPainter(10,10);
--- js/palette.js
+++ js/palette.js
@@ -1,6 +1,6 @@
 
 // Global variable
-var selectedColor;
+var selectedColor = '#000000';
 
 // will create a static grid of static divs
 // hard code all colors into grid
@@ -44,6 +44,6 @@ function colorSelector() {
   console.log(selectedColor);
 }
 
-var colorArry = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086D','#FFFFFF','#CFCFCF','#ABABAB','#838485','#494A4A','#000000','#CB4154','#FF9BAA','#EF98AA','#EE204D','#EE204D','#FC89AC','#FF1DCE','#C364C5','#9D81BA','#7366BD','#5D76CB','#1F75FE'];
+var colorArry = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086','#FFFFFF','#CFCFCF','#ABABAB','#838485','#494A4A','#000000','#CB4154','#FF9BAA','#EF98AA','#EE204D','#EE204D','#FC89AC','#FF1DCE','#C364C5','#9D81BA','#7366BD','#5D76CB','#1F75FE'];
 
 paletteGrid();

0b0f17b78a8375d12db0413569e3a0cea9d1b5a7
BraddahBoots <bryan@thebrocode.io>
2015-09-19 19:14:50 -1000

    Added some css

--- js/mainGrid.js
+++ js/mainGrid.js
@@ -1,42 +1,39 @@
+//setup blank canvas with changeable pixels
+function pixelPainter(width, height) {
+  var rows = height;
+  var cols = width;
+  var row;
+  var pix;
+  var grid = document.createElement('table');
+  grid.id = 'grid';
 
+  //takes in properties of width and height to make matrix grid
+  for(var i = 0; i < rows; i++) {
+    row = document.createElement('tr');
+    row.className = 'row1';
+    for(var j = 0; j < cols; j++) {
+      pix = pixelFactory();
+      row.appendChild(pix);
+    }
+    grid.appendChild(row);
+}
+
+//appends our matrix to the body of HTML
+document.body.appendChild(grid);
+// debugger;
+// console.log("test", document.body.appendChild(grid));
+}
+pixelPainter(100,100);
+
+//create a global variable for our mouse
 var mouseDown = false;
 
+//if our mouse is clicked down - it is true
 document.body.onmousedown = function () {
   mouseDown = true;
 };
 
+//if our mouse is not clicked down = set false
 document.body.onmouseup = function() {
   mouseDown = false;
 };
-
-//setup blank canvas with changeable pixels
-function pixelPainter(width, height) {
-  // width = colums , height = row
-  // matrix of rows * columns
-  // populate with new Pixel --- pixel factory
-  // append each pixel to a unique row
-  // append row to canvas --- pixelPainter
-var rows = height;
-var cols = width;
-var row;
-var pix;
-var grid = document.createElement('table');
-grid.id = 'grid';
-// grid.onmousedown = "dragColor()";
-
-
-for(var i = 0; i < rows; i++) {
-  row = document.createElement('tr');
-  row.className = 'row1';
-  for(var j = 0; j < cols; j++) {
-    pix = pixelFactory();
-    row.appendChild(pix);
-  }
-  grid.appendChild(row);
-}
-
-document.body.appendChild(grid);
-// debugger;
-console.log("test", document.body.appendChild(grid));
-}
-pixelPainter(100,100);
--- js/palette.js
+++ js/palette.js
@@ -2,6 +2,9 @@
 // Global variable
 var selectedColor = '#000000';
 
+// Array of our color options
+var colorArry = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086','#FFFFFF','#CFCFCF','#ABABAB','#838485','#494A4A','#000000','#CB4154','#FF9BAA','#EF98AA','#EE204D','#EE204D','#FC89AC','#FF1DCE','#C364C5','#9D81BA','#7366BD','#5D76CB','#1F75FE'];
+
 // will create a static grid of static divs
 // hard code all colors into grid
 // addeventListener to each pixal color selector
@@ -31,19 +34,55 @@ document.body.appendChild(paletteDiv);
 
 // return clicked pixels color store global
 function palettePixelFactory(color) {
-var pixelColor = document.createElement('td');
-pixelColor.style.background = color;
-pixelColor.style.width = '20px';
-pixelColor.style.height = '20px';
-pixelColor.addEventListener('click', colorSelector);
+  var pixelColor = document.createElement('td');
+  pixelColor.style.background = color;
+  pixelColor.style.width = '20px';
+  pixelColor.style.height = '20px';
+  pixelColor.addEventListener('click', colorSelector);
 return pixelColor;
 }
 
+//selects a color and uses that color to iterate over objects
 function colorSelector() {
   selectedColor = this.style.background;
   console.log(selectedColor);
 }
 
-var colorArry = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086','#FFFFFF','#CFCFCF','#ABABAB','#838485','#494A4A','#000000','#CB4154','#FF9BAA','#EF98AA','#EE204D','#EE204D','#FC89AC','#FF1DCE','#C364C5','#9D81BA','#7366BD','#5D76CB','#1F75FE'];
-
 paletteGrid();
+
+//created IIFI for an eraser button that when clicked erases color objects
+(function eraseButton() {
+  var eraser = document.createElement('button');
+  eraser.id = 'eraser';
+  eraser.innerHTML = "Erase";
+  eraser.style.background = "white";
+  eraser.style.width = '50px';
+  eraser.style.height = '25px';
+  eraser.addEventListener('click', eraseColor);
+  document.body.appendChild(eraser);
+}());
+
+//function set to white color
+function eraseColor() {
+  selectedColor = "#ffffff";
+  console.log('testing',selectedColor);
+}
+
+//created an IIFI for a clear button that when clicked reinvoked our table matrix function
+(function clearButton() {
+  var clearUm = document.createElement('button');
+  clearUm.id = 'clear';
+  clearUm.innerHTML = 'Clear';
+  clearUm.style.background = 'white';
+  clearUm.style.width = '50px';
+  clearUm.style.height = '25px';
+  clearUm.addEventListener('click', clearAll);
+  document.body.appendChild(clearUm);
+}());
+
+//invokes pixelPainter function
+function clearAll() {
+  document.body.removeChild(document.querySelector('#grid'));
+  pixelPainter(100,100);
+}
+

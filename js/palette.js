41ac408ba5163717f84dea6ac294c6827dd9f08f
BraddahBoots <bryan@thebrocode.io>
2015-09-19 14:41:13 -1000

    added color arry

--- js/palette.js
+++ js/palette.js
@@ -6,10 +6,26 @@ var selectedColor;
 // hard code all colors into grid
 // addeventListener to each pixal color selector
 function paletteGrid() {
+var rows = 6;
+var cols = 6;
+var row;
+var paletteDiv = createElement('div');
+var count = 0;
+
+for(var i = 0; i < rows; i++) {
+  row = createElement('div');
+  for(var j = 0; j < cols; j++) {
+
+  }
+
+}
 
 }
 
 // return clicked pixels color store global
-function colorSelector () {
+function colorSelector(color) {
+
 
 }
+
+var colorArry = ['#CD4A4A','#CC6666','#BC5D58','#FD5E53','#FF7538','#9F8170','#FAA76C','#FF8D88','#FFCF48','#FCE883','#BAB86C','#FDFC74','#87A96B','#1DF914','#76FF7A','#6DAE81','#1CAC78','#178086D','#FFFFFF','#CFCFCF','#ABABAB','#838485','#494A4A','#000000','#CB4154','#FF9BAA','#EF98AA','#EE204D','#EE204D','#FC89AC','#FF1DCE','#C364C5','#9D81BA','#7366BD','#5D76CB','#1F75FE'];
